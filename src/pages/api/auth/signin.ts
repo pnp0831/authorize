b2f6f8708ad287c15c1f2afa43f27864a03e1f54
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-03-31 18:22:40 +0700

    .

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     const promises = Object.keys(process.env)
       .filter((item) => item.startsWith("NEXT_PUBLIC_DOMAIN"))
       .map((item) => {
-        fetch(`${NEXT_PUBLIC_DOMAIN}/api/auth/trigger`, {
+        fetch(`${item}/api/auth/trigger`, {
           method: "POST",
           body: {
             token: user.token,
