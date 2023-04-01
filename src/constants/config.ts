00b10b6b0751fcc7adf8c6cf209072ce6e5651cc
Phat Nguyen Pham <phpham@simpsonmfg.com>
2023-04-01 12:16:13 +0700

    .

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,10 @@
 const config = {
   cookieConfig: {
     path: "/",
-    domain:
-      process.env.NODE_ENV === "development"
-        ? "localhost"
-        : process.env.NEXT_PUBLIC_AUTH_URL || ".vercel.app",
+    // domain:
+    //   process.env.NODE_ENV === "development"
+    //     ? "localhost"
+    //     : process.env.NEXT_PUBLIC_AUTH_URL || ".vercel.app",
     httpOnly: false,
   },
 };
